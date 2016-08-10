29da80c27575e4d5fef86ab5691323d68e27add2
Jean D'Amore <jdamore@thoughtworks.com>
2016-08-10 21:55:36 +1000

    Added contract tests with pact and basic spec test to check the routes to registration-service. Also extended preflight to run specs and pacts, rebuild the image, start the container, and ping the health URL

--- registration-service/go.sh
+++ registration-service/go.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export RACK_ENV=production
-export RACK_PORT=3001
+export RACK_PORT=3000
 
 export SERVICE_NAME=$(basename "$PWD")
 echo "Will execute target $1 for $SERVICE_NAME"
@@ -14,13 +14,25 @@ run() {
 	docker run -it --name $SERVICE_NAME --link account-service:account-service -d -p $RACK_PORT:$RACK_PORT --env RACK_PORT=$RACK_PORT $SERVICE_NAME
 }
 
+clean() {
+	docker rm -f $SERVICE_NAME
+}
+
+health() {
+	curl -f http://127.0.0.1:$RACK_PORT
+}
+
 preflight() {	
 	rvm use 2.3.1
 	rvm gemset create $SERVICE_NAME
 	rvm gemset use $SERVICE_NAME
-	gem install bundler
 	bundle install
-	bundle exec rake spec
+	bundle exec rake
+	build
+	run
+	sleep 1
+	health
+	clean
 }
 
 start() {
@@ -29,6 +41,7 @@ start() {
 
 test() {	
 	bundle exec rake spec
+	bundle exec rake pact
 }
 
 if [ $# -eq 0 ]; then
--- registration-service/spec/registration_service_spec.rb
+++ registration-service/spec/registration_service_spec.rb
@@ -1,12 +1,24 @@
 require_relative 'spec_helper'
 
+require 'registration_service'
+require 'rack/test'
+
 describe 'RegistrationService' do
 
-  before do
+  include Rack::Test::Methods
+
+  def app
+    RegistrationService::Api.new
   end
- 
-  it "shoud have a registration endpoint" do
-  	expect(false).to be true
+
+  it "shoud have a GET health endpoint" do
+    get '/'
+    expect(last_response.status).to eq(200)
+  end
+
+  it "shoud have a POST registration endpoint" do
+    post '/'
+    expect(last_response.status).to eq(200)
   end
 
 end
